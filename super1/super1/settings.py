79db27f8dc198a6e77673001e48b47c3b547e446
Blanco <iscblanco@cimat.mx>
2021-03-15 00:23:58 -0600

    React is been loaded successfully

--- super1/super1/settings.py
+++ super1/super1/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'superinfo',
     'rest_framework',
     'superpowers',
+    'frontend',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
--- super1/superinfo/models.py
+++ super1/superinfo/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
 # Create your models here.
 class SuperInformation(models.Model):
-    id_superheroe   = models.IntegerField(primary_key=True)
+    id_superheroe   = models.AutoField(primary_key=True)
     name            = models.CharField(max_length=100)
     alias           = models.CharField(max_length=100)
-    age             = models.IntegerField(max_length=3)
+    age             = models.PositiveIntegerField(validators=[MaxValueValidator(99)])
     sex             = models.CharField(max_length=20)
